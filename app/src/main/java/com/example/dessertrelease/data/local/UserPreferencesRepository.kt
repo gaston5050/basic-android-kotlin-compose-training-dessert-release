4085b8cf23247367cbed621ba5cb53b896658dd0
gaston5050 <80988908+gaston5050@users.noreply.github.com>
2026-07-07 16:30:59 -0300

    Primer commit

---
+++ app/src/main/java/com/example/dessertrelease/data/local/UserPreferencesRepository.kt
@@ -0,0 +1,9 @@
+package com.example.dessertrelease.data.local
+
+import androidx.datastore.core.DataStore
+import java.util.prefs.Preferences
+
+class UserPreferencesRepository(private val dataStore: DataStore<Preferences>) {
+
+
+}
